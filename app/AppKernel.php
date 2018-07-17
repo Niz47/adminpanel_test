d8ad96371aba506bc5f513c16bb77d535bef64f8
Zin Mar Htun <zinmarhtun@reddotnetwork.com>
2018-07-17 17:18:10 +0630

    install symfony assetic and sonata admin bundles

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -22,6 +22,13 @@ class AppKernel extends Kernel
             new FOS\JsRoutingBundle\FOSJsRoutingBundle(),
             new FOS\UserBundle\FOSUserBundle(),
             new Lexik\Bundle\MaintenanceBundle\LexikMaintenanceBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+
+            // enable for sonata admin bundles
+            new Sonata\CoreBundle\SonataCoreBundle(),
+            new Sonata\BlockBundle\SonataBlockBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new Sonata\AdminBundle\SonataAdminBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
